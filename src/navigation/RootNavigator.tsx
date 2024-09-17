e9a1c24723303c88d22db6df89eae145292cec40
Julian Lasso <julian.lasso@sofka.com.co>
2024-09-17 00:05:16 -0500

    feat: :sparkles: se da soporte a la autenticación y navegación bajo la misma

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,33 +1,44 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {useAuth} from '../context/AuthContext';
 import LoginScreen from '../screens/Login';
+import SignUpScreen from '../screens/SignUp';
 import TabNavigator from './TabNavigator';
 
 export type RootStackParamList = {
   Login: undefined;
+  SignUp: undefined;
   TabNavigator: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
+  const {isAuthenticated} = useAuth();
+
   return (
     <Stack.Navigator
-      initialRouteName={'Login'}
+      initialRouteName={isAuthenticated ? 'TabNavigator' : 'Login'}
       screenOptions={{
         headerShown: false,
       }}>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen
-        name="TabNavigator"
-        component={TabNavigator}
-        options={{
-          title: 'My Awesome App',
-          headerShown: true,
-          headerBackVisible: false,
-          headerShadowVisible: false,
-        }}
-      />
+      {!isAuthenticated ? (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+        </>
+      ) : (
+        <Stack.Screen
+          name="TabNavigator"
+          component={TabNavigator}
+          options={{
+            title: 'My Awesome App',
+            headerShown: true,
+            headerBackVisible: false,
+            headerShadowVisible: false,
+          }}
+        />
+      )}
     </Stack.Navigator>
   );
 };
