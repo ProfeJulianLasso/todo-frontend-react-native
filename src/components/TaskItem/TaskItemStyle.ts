6e503c38584294c7fc5f1f0a27088efe7ebb8c78
Julian Lasso <julian.lasso@sofka.com.co>
2024-09-17 00:24:41 -0500

    feat: :sparkles: se agrega componente para mostrar cada tarea en un listado

---
+++ src/components/TaskItem/TaskItemStyle.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const taskItemStyle = StyleSheet.create({
+  taskContainer: {
+    marginHorizontal: 16,
+    marginBottom: 16,
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    backgroundColor: '#ffffff',
+  },
+  taskTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
---
+++ src/components/TaskItem/index.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+import {taskItemStyle} from './TaskItemStyle';
+
+// Definición de las propiedades que el componente TaskItemComponent recibirá
+interface TaskItemComponentProps {
+  title: string; // Título de la tarea
+  description: string; // Descripción de la tarea
+  onLongPress: () => void; // Función que se ejecuta al mantener presionado el elemento
+}
+
+// Componente de elemento de tarea funcional
+const TaskItemComponent: FC<TaskItemComponentProps> = ({
+  title,
+  description,
+  onLongPress,
+}) => {
+  return (
+    <TouchableOpacity
+      onLongPress={onLongPress} // Maneja el evento de mantener presionado
+      style={taskItemStyle.taskContainer}>
+      {/* Estilo del contenedor de la tarea */}
+      <Text style={taskItemStyle.taskTitle}>{title}</Text>
+      {/* Muestra el título de la tarea */}
+      <Text>{description}</Text> {/* Muestra la descripción de la tarea */}
+    </TouchableOpacity>
+  );
+};
+
+export default TaskItemComponent; // Exporta el componente TaskItemComponent
