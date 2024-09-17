c064410742e864885b610488ebe5e508e7e9b21c
Julian Lasso <julian.lasso@sofka.com.co>
2024-09-16 22:26:11 -0500

    feat: :construction: instalación y configuración del paquete react-native-config

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,10 +1,12 @@
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import React, {ReactElement} from 'react';
 import {Button, Text, View} from 'react-native';
+import Config from 'react-native-config';
 import {RootStackParamList} from '../../navigation/RootNavigator';
 
 const LoginScreen = (): ReactElement => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  console.log('LoginScreen', Config.HOST);
 
   return (
     <View>
